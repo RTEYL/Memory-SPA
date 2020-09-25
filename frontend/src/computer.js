ff0a30454433ceeef4b1355c1eab6b54c36a848a
RTEYL <tyler-w-jones@hotmail.com>
2020-09-24 17:24:26 -0700

    remove extra points on difficulty

--- frontend/src/computer.js
+++ frontend/src/computer.js
@@ -9,17 +9,14 @@ class Computer {
 			case 'Hard':
 				this.intValCount = 800;
 				this.boxCount = 12;
-				this.extraPointCount = 3;
 				break;
 			case 'Medium':
 				this.intValCount = 1400;
 				this.boxCount = 9;
-				this.extraPointCount = 2;
 				break;
 			default:
 				this.intValCount = 2000;
 				this.boxCount = 6;
-				this.extraPointCount = 1;
 				break;
 		}
 	}
